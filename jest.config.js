18b6778b240886cc6f62f5290dd9a6538a0716db
HallexCosta <hallex.costa@hotmail.com>
2021-03-13 01:43:26 -0300

    feat(repo): add path projects for shared packages

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
-  projects: ['<rootDir>/packages/**/jest.config.js'],
+  projects: [
+    '<rootDir>/packages/**/jest.config.js',
+    '<rootDir>/packages/shared/**/jest.config.js'
+  ],
   testEnvironment: 'node',
   testMatch: ['*.spec.ts'],
   transform: {
