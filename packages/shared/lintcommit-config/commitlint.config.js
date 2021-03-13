0c0e1800041d0f96f9a89bca66b89559bee69c2c
HallexCosta <hallex.costa@hotmail.com>
2021-03-13 14:19:19 -0300

    feat(repo): add lintcommit config

---
+++ packages/shared/lintcommit-config/commitlint.config.js
@@ -0,0 +1,33 @@
+const { join } = require('path')
+const { readdirSync: readDirectory } = require('fs')
+const DEFAULT_SCOPES = ['repo']
+
+const packageDirNames = readDirectory(
+  join(__dirname, '..', '..', '..', 'packages'),
+  { withFileTypes: true }
+)
+  .filter(entry => entry.isDirectory())
+  .map(dir => (dir.name === 'shared' ? null : dir.name))
+  .filter(name => name != null)
+
+const packagesSharedDirNames = readDirectory(
+  join(__dirname, '..', '..', '..', 'packages', 'shared'),
+  { withFileTypes: true }
+)
+  .filter(entry => entry.isDirectory())
+  .map(dir => dir.name)
+
+const allPackagesDIrNames = packageDirNames.concat(packagesSharedDirNames)
+
+const scopes = DEFAULT_SCOPES.concat(allPackagesDIrNames)
+
+module.exports = {
+  extends: ['monorepo'],
+  rules: {
+    'scope-enum': [
+      2, // throw error
+      'always',
+      scopes
+    ]
+  }
+}
