baf63dd98ebbe8a315c9f4068ea6cd6487c170f1
HallexCosta <hallex.costa@hotmail.com>
2021-03-30 23:53:24 -0300

    test(observer): add observer tests

---
+++ packages/observer/__tests__/unit/Observer.spec.ts
@@ -0,0 +1,41 @@
+import { Observable, Observer } from '@implementations/index'
+
+jest.mock('../../src/implementations/Observable', () => Observable)
+
+const ObservableMock = Observable as jest.Mock<Observable>
+
+function factoryObserver() {
+  const instance = new Observer()
+  const observerIdentifier = instance.identifier()
+
+  return {
+    instance,
+    observerIdentifier
+  }
+}
+describe('Observer', () => {
+  it('Should be able to generate a unique hash for each instance of Observer', () => {
+    const { observerIdentifier } = factoryObserver()
+    const { observerIdentifier: observerIdentifier2 } = factoryObserver()
+
+    expect(observerIdentifier.hashId.length).toEqual(24)
+    expect(typeof observerIdentifier.symbolId).toBe('symbol')
+    expect(observerIdentifier.hashId).not.toEqual(observerIdentifier2.hashId)
+    expect(observerIdentifier.symbolId).not.toEqual(
+      observerIdentifier2.symbolId
+    )
+  })
+
+  it('Should be able to notify observer for make the tasks implemented on method', () => {
+    const observer = new Observer()
+    const observable = new ObservableMock()
+
+    const observerIdentifier = observer.identifier()
+
+    observer.notify(observable)
+
+    expect(observerIdentifier.hashId).toEqual(
+      observable.observerIdentifier.hashId
+    )
+  })
+})
